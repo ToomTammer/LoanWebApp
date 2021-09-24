1b54518f8f60cbdc5d5561b996da4797e118f41a
ToomTammer <sm.suparerk@gmail.com>
2021-09-25 04:05:50 +0700

    Change

---
+++ Loan Webapp/Models/AnnotationsCal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loan_Webapp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loan_Webapp.Models
+{
+    public class AnnotationsCal
+    {
+        [Key]
+
+        [Required(ErrorMessage = "กรุณาป้อนจำนวนเงิน")]
+        [StringLength(9, MinimumLength = 6, ErrorMessage = "กรุณาป้อนจำนวนเงินอย่างน้อย 6 หลัก เเละ ไม่เกิน 9 หลัก")]
+        public string Inputmoney { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนระยะเวลา")]
+        [StringLength(2, MinimumLength = 1, ErrorMessage = "กรุณาป้อนระยะเวลาให้ถูกต้อง")]
+        public string Inputyrs { get; set; }
+    }
+}
